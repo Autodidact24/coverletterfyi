d0bafe211308fe2e8258f1590c3bd9f1e7a8f891
Shubham Tomar <tomarshubham24@gmail.com>
2023-05-15 17:47:51 +0530

    changed domain name

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9f54887f90503220"; }
+/******/ 		__webpack_require__.h = function() { return "909ba72291f321e3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
